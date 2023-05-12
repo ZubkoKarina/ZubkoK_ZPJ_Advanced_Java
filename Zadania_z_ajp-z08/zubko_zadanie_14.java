c03f6af0d9e79d5ab340a4c2f20f2fe215973c69
Karina Zubko <karinazubko2017@gmail.com>
2023-05-12 09:52:33 +0200

    Add Java program to concatenate arraylists

---
+++ Zadania_z_ajp-z08/zubko_zadanie_14.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class zubko_zadanie_14 {
+    public static void main(String[] args) {
+        Stream<ArrayList<Integer>> stream = Stream.of(
+                new ArrayList<>(Arrays.asList(1, 2, 3)),
+                new ArrayList<>(Arrays.asList(4, 5, 6)),
+                new ArrayList<>(Arrays.asList(7, 8, 9))
+        );
+        ArrayList<Integer> result = stream.reduce(new ArrayList<>(), (a, b) -> {
+            a.addAll(b);
+            return a;
+        });
+        System.out.println(result);
+    }
+}
